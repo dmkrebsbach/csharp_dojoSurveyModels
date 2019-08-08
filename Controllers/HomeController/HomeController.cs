4611771d1a9263883d557a1409c5ef1579a0e022
dmkrebsbach <damiank@uw.edu>
2019-08-08 13:25:45 -0700

    validations added to Dojo Survey Model

--- Controllers/HomeController/HomeController.cs
+++ Controllers/HomeController/HomeController.cs
@@ -13,8 +13,27 @@ namespace dojoSurveyModel
             return View("Index");
         }
 
-        [HttpPost("result")] //Post Method from the Form
-        public IActionResult Results(Survey newSurvey)
+        [HttpPost("createSurvey")]
+        public IActionResult Create(Survey newSurvey)
+        {
+            Console.Write("Is This Getting Here?");
+            if(ModelState.IsValid)
+            {
+                Console.Write("###########It's Valid?#########");
+                // do somethng!  maybe insert into db?  then we will redirect
+                return RedirectToAction("Success", newSurvey);
+            }
+            else
+            {
+                Console.Write("##########It's Not Valid?#########");
+                // Oh no!  We need to return a ViewResponse to preserve the ModelState, and the errors it now contains!
+                return View("Index");
+            }
+        }
+
+
+        [HttpGet("Results")] //Post Method from the Form
+        public IActionResult Success(Survey newSurvey)
         {
             // This recieves a POST from the HTML Form, and grabs the "Survey" class and makes a new instance of the class.
             return View("Results", newSurvey); // returns the information mapped from the class and form
--- Models/Survey.cs
+++ Models/Survey.cs
@@ -1,9 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace dojoSurveyModel.Models{
     public class Survey
     {
+        [Required(ErrorMessage = "Please enter your First name")]
+        [MinLength(4, ErrorMessage = "First Name must be at least 4 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Select a Dojo Location")]
         public string Dojo { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Favorite Language")]
         public string Language{ get; set; }
+
+        [MaxLength(20, ErrorMessage = "Keep your comment to 20 Characters or Less")]
         public string Comment{ get; set; }
         
     }
